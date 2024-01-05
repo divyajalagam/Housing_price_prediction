96a57836b8c8e9f784075968c8c8ebd4b1fae3a6
Sai Divya Jalagam <105504131+divyajalagam@users.noreply.github.com>
2024-01-05 08:57:06 +0530

    Update house_price.py

--- house_price.py
+++ house_price.py
@@ -38,7 +38,7 @@ property_type_mapping = {"Apartment": 0,
                          
                          
 #reading pickle file
-with open("model.pkl",'rb')as f:
+with open("Model.pkl",'rb')as f:
     model=pickle.load(f)
     
     
